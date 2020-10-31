9cccde239a74d355175d6392be0efb1fda405200
DESKTOP-3I019BE\gwjung <charminggw@gmail.com>
2020-10-31 22:01:57 +0900

    create SortAlgorithm Interface and use it instead of implements

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -1,13 +1,18 @@
 package com.in28minutes.spring.basics.springin5steps;
 
 public class BinarySearchImpl {
+	// 구현체 대신 인터페이스를 사용한다 (-> 결합이 느슨해졌기 때문에 자유롭게 원하는 알고리즘을 선택할 수 있다)
+	private SortAlgorithm sortAlgorithm;
 	
-	// bubbleSortAlgorithm에서 QuickSortAlgorithm으로 바꾸고 싶을때?? => Interface 이용하기
-	public int binarySearch(int[] numbers, int numberToSearchFor) {
+	public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
+		super();
+		this.sortAlgorithm = sortAlgorithm;
+	}
 
-		BubbleSortAlgorithm bubbleSortAlgorithm = new BubbleSortAlgorithm();
+	public int binarySearch(int[] numbers, int numberToSearchFor) {
 		
-		int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
+		int[] sortedNumbers = sortAlgorithm.sort(numbers);
+		System.out.println(sortAlgorithm);
 	
 		// Search the array
 		
--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -13,13 +13,15 @@ public class SpringIn5StepsApplication {
 		 * injection
 		 */
 		
-		BinarySearchImpl binarySearch = new BinarySearchImpl();
+		// if you wanna change quickSort all that i need to do is change it here.
+		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
 		
 		int result = binarySearch.binarySearch(new int[] {12,4,6}, 3);
+		//com.in28minutes.spring.basics.springin5steps.BubbleSortAlgorithm@7de26db8
 		
 		System.out.println(result);
 
-		SpringApplication.run(SpringIn5StepsApplication.class, args);
+		//SpringApplication.run(SpringIn5StepsApplication.class, args);
 	}
 
 }
