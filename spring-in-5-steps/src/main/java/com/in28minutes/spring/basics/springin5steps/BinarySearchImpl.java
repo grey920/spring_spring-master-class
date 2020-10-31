dcafce8c31ddd2dac3c956c28d10c7d810e00aa2
DESKTOP-3I019BE\gwjung <charminggw@gmail.com>
2020-10-31 21:47:43 +0900

    create QuickSortAlgorithm

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -2,7 +2,7 @@ package com.in28minutes.spring.basics.springin5steps;
 
 public class BinarySearchImpl {
 	
-
+	// bubbleSortAlgorithm에서 QuickSortAlgorithm으로 바꾸고 싶을때?? => Interface 이용하기
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
 
 		BubbleSortAlgorithm bubbleSortAlgorithm = new BubbleSortAlgorithm();
