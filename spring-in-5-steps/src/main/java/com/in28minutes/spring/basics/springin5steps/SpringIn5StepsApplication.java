79dc2bde870480d5a5ba315cf6b4f2bf469b04dd
DESKTOP-3I019BE\gwjung <charminggw@gmail.com>
2020-11-02 19:24:54 +0900

    Manage Dependencies

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -13,13 +13,19 @@ public class SpringIn5StepsApplication {
 	// Where to search for beans? => No need
 	public static void main(String[] args) {
 	
-		//스프링이 알아서 만들어 줘서 필요없다.
+		//우리가 직접 빈을 만들어서 와이어링 한 버전
 		//BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
 		//Application Context
+		
+		// 스프링이 대신 와이어링 한 것
 		ConfigurableApplicationContext applicationContext = 
 				SpringApplication.run(SpringIn5StepsApplication.class, args); //<- by default
 		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
 		int result = binarySearch.binarySearch(new int[] {12,4,6}, 3);
+		System.out.println(result);
+		//com.in28minutes.spring.basics.springin5steps.BubbleSortAlgorithm@43c67247
+		//3
+
 	}
 
 }
