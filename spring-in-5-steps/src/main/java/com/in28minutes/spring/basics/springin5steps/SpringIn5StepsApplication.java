3ade544ccb7e8ed5de9a8705e3def2ce29ddb88c
DESKTOP-3I019BE\gwjung <charminggw@gmail.com>
2020-11-02 19:19:09 +0900

    application context

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -3,23 +3,23 @@ package com.in28minutes.spring.basics.springin5steps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringIn5StepsApplication {
 
-	// What are the beans?
-	// What are the dependencies of a bean?
+	// What are the beans? => @Component
+	// What are the dependencies of a bean? => @Autowired
 	// Where to search for beans? => No need
 	public static void main(String[] args) {
 	
-		// 느슨한 결합 (생성자를 통한 주입)
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
-		
+		//스프링이 알아서 만들어 줘서 필요없다.
+		//BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+		//Application Context
+		ConfigurableApplicationContext applicationContext = 
+				SpringApplication.run(SpringIn5StepsApplication.class, args); //<- by default
+		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
 		int result = binarySearch.binarySearch(new int[] {12,4,6}, 3);
-		
-		System.out.println(result);
-
-		//SpringApplication.run(SpringIn5StepsApplication.class, args);
 	}
 
 }
