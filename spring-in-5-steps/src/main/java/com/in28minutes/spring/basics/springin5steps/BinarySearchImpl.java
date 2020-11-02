0a47953eeab263ad378782b3a4c0a5737c29803e
DESKTOP-3I019BE\gwjung <charminggw@gmail.com>
2020-11-02 18:57:28 +0900

    2. What are the dependencies of a bean?

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -1,10 +1,13 @@
 package com.in28minutes.spring.basics.springin5steps;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 //1. 스프링에게 BinarySearchImpl이 빈이라고 알려준다 (by adding @Component)
 @Component
 public class BinarySearchImpl {
+	//2. 생성한 빈 안에 무엇이 디펜던시인지 알려준다 (by adding @Autowired)
+	@Autowired
 	private SortAlgorithm sortAlgorithm;
 	
 	public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
