ee7dd5844e97d63e4d8fce073417d1d1d3dd7b4a
DESKTOP-3I019BE\gwjung <charminggw@gmail.com>
2020-11-07 17:22:18 +0900

    Autowiring by Name

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -3,22 +3,16 @@ package com.in28minutes.spring.basics.springin5steps;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-//1. 스프링에게 BinarySearchImpl이 빈이라고 알려준다 (by adding @Component)
 @Component
 public class BinarySearchImpl {
-	//2. 생성한 빈 안에 무엇이 디펜던시인지 알려준다 (by adding @Autowired)
+	// 1. Autowiring by name
 	@Autowired
-	private SortAlgorithm sortAlgorithm;
-	
-	public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
-		super();
-		this.sortAlgorithm = sortAlgorithm;
-	}
+	private SortAlgorithm bubbleSortAlgorithm;
 
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
 		
-		int[] sortedNumbers = sortAlgorithm.sort(numbers);
-		System.out.println(sortAlgorithm);
+		int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
+		System.out.println(bubbleSortAlgorithm);
 	
 		// Search the array
 		
--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BubbleSortAlgorithm.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BubbleSortAlgorithm.java
@@ -1,7 +1,7 @@
 package com.in28minutes.spring.basics.springin5steps;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
-
 @Component
 public class BubbleSortAlgorithm implements SortAlgorithm{
 	public int[] sort(int[] numbers) {
